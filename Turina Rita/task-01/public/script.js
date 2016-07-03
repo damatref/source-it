92ee8e0b54ce2ff0741de988fa005ba0dbfdd55e
Rita Turina <malgojata@ya.ru>
2016-07-04 01:13:14 +0300

    geolocation and workers task - tabs half started

---
+++ Turina Rita/task-01/public/script.js
@@ -0,0 +1,53 @@
+(function () {
+
+    // function tabs(selector) {
+    //     var item = $(selector).find('.active a');
+    //     showTab(item);
+
+    //     $(selector).find('a').click(function(ev) {
+    //         ev.preventDefault();
+    //         if (!$(this).parent().hasClass('active'))
+    //            {showTab($(this));}        
+    //    })
+    // }
+    // function showTab(item){
+    //     var itemData = $(item).data('tabholder');
+
+    //     $(selector).find('li').removeClass('active');
+    //     $('.tab-content').hide();
+
+    //     $(item).parent().addClass('active');
+    //     $(itemData).show('slow');
+    // }
+
+
+    'use strict';
+
+    var ITERATIONS = 100000000;
+
+    //Function that generates random coordinates for point(x:[-r,r), y:[-r,r))
+    //and checks if it is in a circle with radius r
+    var generatePoint = function () {
+        var r = 16;
+        var x = Math.random() * r * 2 - r;
+        var y = Math.random() * r * 2 - r;
+        return (Math.pow(x, 2) + Math.pow(y, 2) < Math.pow(r, 2))
+    };
+
+    //Return estimated value of Pi after all iterations
+    var computePi = function () {
+        var inCircle = 0;
+        var i;
+        for (i = 0; i < ITERATIONS; i++) {
+            if (generatePoint()) {
+                inCircle++;
+            }
+        }
+        return inCircle / ITERATIONS * 4;
+    };
+
+    //Performs synchronous calculations of Pi after click on button
+    document.querySelector('#syncstart').addEventListener('click', function () {
+        document.querySelector('#syncresult').innerHTML = computePi();
+    });
+})();
---
+++ Turina Rita/task-01/readme.txt
@@ -0,0 +1,42 @@
+DESCRIPTION:
+
+Implement tabbed page and implement tabs functionality with use of History, Fullscreen, Geolocation and WebWorker APIs according to requirement in application-requirement.md
+
+
+REQUIREMENTS:
+
+- Installed node.js server and npm dependencies for project
+
+    1. Download and install Node.js from http://nodejs.org/
+    2. Open folder with package.json in console
+    3. Run: npm install
+    4. Run npm start
+    5. Open http://localhost:3001/ in browser for test
+
+- Support all major desktop browsers:
+   	* Chrome,
+   	* Firefox,
+   	* IE11
+   	* Edge
+
+
+WORKFLOW:
+
+Upload implemented layout to git in folder "students/<first_last-name>/<name of module>/<name of topic>/task-01"
+
+Structure of the task should be:
+```
+task-01
+   	public
+   	    index.html
+   		script.js
+   		styles.css
+    package.json
+    server.js
+```
+
+SOURCES:
+
+```wireframes/``` - folder with wireframes each tab
+```server.js``` - server application
+```public folder``` - prepared files for your markup, scripts, styles, and images.
